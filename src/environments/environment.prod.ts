5a948f9d71168c02ef977a8e463934c3cb4c57ca
ANAM DIMC <dev@srv827868.hstgr.cloud>
2026-01-15 15:35:39 -0600

    commit rama recovery

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseurl: '',
+  baseurl: '/api/',  
   baseurlAssets: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
-
 export const environment = {
   production: false,
-  baseurl: 'http://89.116.51.124:9010/',
+  baseurl: '/api/',
   baseurlAssets: '',
 };
-
